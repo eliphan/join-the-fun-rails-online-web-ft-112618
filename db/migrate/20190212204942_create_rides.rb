0e27cc7e50d099febd87c51216eb3aa0ab02c8fc
Elise Phan <aliceconnic@gmail.com>
2019-02-12 20:58:08 +0000

    Automatically backed up by Learn

--- db/migrate/20190212204942_create_rides.rb
+++ db/migrate/20190212204942_create_rides.rb
@@ -1,6 +1,6 @@
 class CreateRides < ActiveRecord::Migration
   def change
-    create_join_table :taxis, :passengers, table_name: :taxis_passengers do |t|
+    create_join_table :taxis, :passengers, table_name: :rides do |t|
        t.index :taxi_id
        t.index :passenger_id
     end
