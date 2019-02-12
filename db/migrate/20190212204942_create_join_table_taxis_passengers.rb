4df2fa1c5f9ffa68f8b357bf1468cedeb2645288
Elise Phan <aliceconnic@gmail.com>
2019-02-12 20:56:17 +0000

    Automatically backed up by Learn

--- db/migrate/20190212204942_create_join_table_taxis_passengers.rb
+++ db/migrate/20190212204942_create_join_table_taxis_passengers.rb
@@ -1,6 +1,6 @@
 class CreateJoinTableTaxisPassengers < ActiveRecord::Migration
   def change
-    create_join_table :taxis, :passengers do |t|
+    create_join_table :taxis, :passengers, table_name: do |t|
        t.index :taxi_id
        t.index :passenger_id
     end
