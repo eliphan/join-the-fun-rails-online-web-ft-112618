c0431e117de940c8bcb373c082fc152f31b40ed0
Elise Phan <aliceconnic@gmail.com>
2019-02-12 21:09:33 +0000

    Automatically backed up by Learn

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -1,9 +1,7 @@
 class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
-       t.integer :taxi_id
-       t.integer :passenger_id
-       
+
       t.timestamps null: false
     end
   end
